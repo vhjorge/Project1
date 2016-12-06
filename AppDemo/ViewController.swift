0b3203137473d72433cd376c55dd865fbff8c3d3
AdminUTMVHSA <AdministradorUTM1@static-201-151-105-52.alestra.net.mx>
2016-12-06 09:53:22 -0600

    Se agregaron métodos tableView

--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate{
+    
+    let datos = [("Juan",45),("Juan1",40),("Juan2",25)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return datos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let proto = (indexPath.row % 2 == 0) ? "Proto1" : "Proto2"
+        let vista = tableView.dequeueReusableCell(withIdentifier: proto, for: indexPath) as! FilaTableViewCell
+        vista.Izquierda.text = datos[indexPath.row].0
+        vista.Derecha.text = "\(datos[indexPath.row].1)"
+        return vista
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print("Fila \(indexPath.row)")
+    }
+    
+}
 
+class clase{
+    var numero = 1
+        
 }
 
