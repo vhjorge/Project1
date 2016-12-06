0859851569054c578c28939f43fdde270c79a099
AdminUTMVHSA <AdministradorUTM1@static-201-151-105-52.alestra.net.mx>
2016-12-06 15:29:45 -0600

    Cambios 6 de Diciembre

--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,26 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate{
-    
-    let datos = [("Juan",45),("Juan1",40),("Juan2",25)]
 
+class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate, DetalleViewControllerDelegate, AgregarViewControllerDelegate{
+    
+    var datos = [("Juan",45),("Juan1",40),("Juan2",25)]
+    var indexDato = -1
+    
+    func numeroCambiado(numero: Int) {
+        print("Nùmero Cambiado: \(numero)")
+        datos[numero].1 = datos[numero].1 + 1
+        tblDatos.reloadData()
+    }
+    
+    func agregarPersona(nombre: String, edad: Int, fila : Int) {
+        if (fila == -1)
+        {
+            datos.append((nombre,edad));
+        }
+        else
+        {
+            datos[fila].0 = nombre
+            datos[fila].1 = edad
+        }
+        tblDatos.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func btnAdd(_ sender: UIBarButtonItem) {
+        indexDato = -1
+        performSegue(withIdentifier: "AgregarSegue", sender: self)
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return datos.count
     }
     
+    
+    @IBOutlet weak var tblDatos: UITableView!
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
@@ -40,8 +68,52 @@ class ViewController: UIViewController, UITableViewDataSource , UITableViewDeleg
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        print("Fila \(indexPath.row)")
+        //print("Fila \(indexPath.row)")
+        indexDato = indexPath.row
+        performSegue(withIdentifier: "DetalleSegue", sender: self)
+        
     }
+    
+    func editarFila(sender : UITableViewRowAction, indexpath : IndexPath){
+        indexDato = indexpath.row
+        performSegue(withIdentifier: "AgregarSegue", sender: self)
+        
+    }
+    
+    func borrarFila(sender : UITableViewRowAction, indexpath : IndexPath){
+        datos.remove(at: indexpath.row)
+        tblDatos.reloadData()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let eliminar = UITableViewRowAction(style: .destructive, title: "Borrar", handler: borrarFila)
+        let editar = UITableViewRowAction(style: .normal, title: "Editar", handler: editarFila)
+        return [editar, eliminar]
+    }
+    
+    
+    // MARK: UIViewDelagates
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "DetalleSegue"){
+            
+            let view = segue.destination as! DetalleViewController
+            view.numerofila = indexDato
+            view.delegado = self
+            view.DatoSelecccionado[0] = ("\(datos[indexDato].0)" , datos[indexDato].1)
+        }
+        if (segue.identifier == "AgregarSegue"){
+            
+            let view = segue.destination as! AgregarViewController
+            view.delegado = self
+            if (indexDato > -1){
+                view.DatoSelecccionado[0] = ("\(datos[indexDato].0)" , datos[indexDato].1)
+            }
+            view.fila = indexDato
+            
+        }
+    }
+    
     
 }
 
