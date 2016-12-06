102af050d0456d58e53704461be721df7f4e6f15
AdminUTMVHSA <AdministradorUTM1@static-201-151-105-52.alestra.net.mx>
2016-12-06 09:52:00 -0600

    FilaTableViewCell.swift

---
+++ FilaTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FilaTableViewCell.swift
+//  AppDemo
+//
+//  Created by AdminUTMVHSA on 05/12/16.
+//  Copyright © 2016 AdminUTMVHSA. All rights reserved.
+//
+
+import UIKit
+
+class FilaTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var Derecha: UILabel!
+    @IBOutlet weak var Izquierda: UILabel!
+    
+    
+}
